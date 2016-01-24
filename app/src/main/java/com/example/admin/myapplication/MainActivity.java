841e1c5e9e48e89a7e31fab701d3b505fa782ea0
Rem Mahnach <rem9020@gmail.com>
2016-01-24 21:22:14 +0200

    Added button animation

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.admin.myapplication;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.admin.myapplication.Nodes.Node;
+
 import com.example.admin.myapplication.Nodes.Queue;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.Random;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
+
 
     //Order queue stores the order the buttons need to be pressed
     //Will only be added values to
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     //3 = Red
     Button[] Btn;
     MediaPlayer[] Sounds;
+    Activity mActivity;
 
 
     @Override
@@ -52,53 +57,64 @@ public class MainActivity extends AppCompatActivity {
         Sounds[1] = MediaPlayer.create(this,R.raw.b);
         Sounds[2] = MediaPlayer.create(this,R.raw.c);
         Sounds[3] = MediaPlayer.create(this,R.raw.d);
+        mActivity = this;
     }
 
     public void Green(View v) {
-
+       PressAnim(0);
     }
 
     public void Blue(View v) {
-
+        PressAnim(1);
     }
 
     public void Yellow(View v) {
-
+        PressAnim(2);
     }
 
     public void Red(View v) {
-
+        PressAnim(3);
     }
 
-    //Called by Computer to show what buttons to click
-    public void changestate(int ButtonId,int DrawableId ){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            Btn[ButtonId].setBackground(getResources().getDrawable(DrawableId));
-            Sounds[ButtonId].start();
-            
+    //Cycles through a button's click animation
+   public void PressAnim( final int ButtonId){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Btn[ButtonId].setPressed(true);
+                    Sounds[ButtonId].start();
+                    //If Order of numbers isn't first
+                    if (Order.getFirst().getNext() != null){
 
-        }
-        else {
-            Btn[ButtonId].setBackgroundDrawable(getResources().getDrawable(DrawableId));
-        }
-    }
+                    }
+                    try{
+                        //Try pausing button for .5 seconds
+                        Thread.sleep(100);
+                    }catch (InterruptedException e){
+                        //If problem occurs than print stack trace
+                        e.printStackTrace();
+                    }
+                        Btn[ButtonId].invalidate();
+                    }
+                }
+
+                );
+            }
 
 
     public void Play(View v) {
         Order = new Queue<>();
         Integer Rando = NewButt.nextInt(4); //Takes out random number between 0 and 4
-        Order.size();
         Order.Push(Rando);//Pushes random number into the Order Queue
         Temp = Order;// saves Order queue into Temp Queue
+        //Cycles through press animation of chosen button
         switch (Rando){
-            case 0:changestate(Rando, R.drawable.greenpressed);break;
-            case 1:changestate(Rando, R.drawable.bluepressed);break;
-            case 2:changestate(Rando, R.drawable.yellowpressed);break;
-            case 3:changestate(Rando, R.drawable.redpressed);break;
+            case 0:PressAnim(Rando);break;
+            case 1:PressAnim(Rando);break;
+            case 2:PressAnim(Rando);break;
+            case 3:PressAnim(Rando);break;
         }
+
         v.setClickable(false);
         }
-    public void show(){
-
-    }
     }
